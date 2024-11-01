5cad01e25427b5ed2b33662f5f28d3b60c6d41c7
Karthik-M11 <karthikmanoranjan11@gmail.com>
2024-11-01 17:56:36 +0530

    updated controller and gui

--- controller_sim.py
+++ controller_sim.py
@@ -1,68 +1,60 @@
-
 import math as m
-import time 
-import numpy as np 
-from scipy.integrate import odeint 
-import matplotlib.pyplot as plt 
+import time
+import numpy as np
+from scipy.integrate import odeint
+from PyQt5.QtCore import pyqtSignal, QThread
 from simple_pid import PID
-from threading import Thread
+import serial
 
+class DifferentialEqnThread(QThread):
+    update_height = pyqtSignal(float)
 
-class Controller:
-    def __init__(self,):
+    def __init__(self, set_point_height=0.025):
+        super().__init__()
         self.stop_sim = False
-        self.set_point_height = 0.025
-        self.fp_sim_hist = []
-        self.real_pcs_hist = []
-
-        pass
-    
-    def run_fp_model_sim_async(self,):
-        fpsim_thread = Thread(target = self.fp_model_sim)
-        fpsim_thread.start()
-        fpsim_thread.join()
-    def run_real_pcs_async(self,):
-        real_pcs_thread = Thread(target = self.real_pcs)
-        real_pcs_thread.start()
-        real_pcs_thread.join()
+        self.set_point_height = set_point_height
 
 
+    def run(self):
+        def fp_model(h, t, v):
+            fmin = 0.022
+            fmax = 0.033
+            f = fmax * v / 1023
+            PI = m.pi
+            d = 0.008
+            r = 0.185
+            h0 = 0.025        
+            dhdt = (f - (0.6 * (PI * pow(d, 2)) * m.sqrt(2 * 9.81 * (h - h0)))) / (PI * (2 * r * h - pow(h, 2)))
+            return dhdt
 
-    def fp_model_sim(self):
-        st = time.time()
-
-        def fp_model(h,t,v):
-            fmin = 0.022#L/s
-            fmax = 0.033#L/s
-            f    = fmax*v/1023 #l/s
-            PI   = m.pi
-            d    = 0.008 #m
-            r    = 0.185 #m
-            h0   = 0.025 #m        
-            dhdt = (f-(0.6*(PI*pow(d,2))*m.sqrt(2*9.81*(h-h0))))/(PI*(2*r*h - pow(h,2)))
-            return dhdt   
-
-        h_init = 0.025 #m
-        h_max  = 0.18  #m 
+        h_current = 0.025
         pid = PID(30.0, 1, 0, setpoint=self.set_point_height)
-        pid.output_limits = (0,6)
-        h_current = h_init
+        pid.output_limits = (0, 6)
 
         while not self.stop_sim:
-            if time.time() - st > 1:
-                pid.setpoint = self.set_point_height
-                t_now     = time.time()
-                t         = np.asarray([0.0, t_now])
-                v         = pid(h_current)
-                print(f"height : {h_current} , voltage : {v} ")
-                h         = odeint(fp_model, h_init, t, (v,))
-                h_current = h[-1]
-                st = t_now
-                self.fp_sim_hist.append((t_now,h_current))
+            time.sleep(1)
+            pid.setpoint = self.set_point_height
+            v = pid(h_current)
+            t = [0.0, 1.0]
+            h = odeint(fp_model, h_current, t, args=(v,))
+            h_current = h[-1][0]
+            self.update_height.emit(h_current)
+            print(self.set_point_height)
 
-        pass
-    def real_pcs(self):
-        SERIAL_PORT = '/dev/ttyUSB0'  # Update to your serial port
+    def stop(self):
+        self.stop_sim = True
+
+
+class RealSystemThread(QThread):
+    update_height = pyqtSignal(float)
+
+    def __init__(self, set_point_height=0.025):
+        super().__init__()
+        self.stop_sim = False
+        self.set_point_height = set_point_height
+
+    def run(self):
+        SERIAL_PORT = '/dev/ttyUSB0'
         BAUD_RATE = 9600
         arduino = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
 
@@ -70,28 +62,45 @@ class Controller:
             if arduino.in_waiting > 0:
                 line = arduino.readline().decode('utf-8').strip()
                 return line
-        
+            return None
+
         pid = PID(30.0, 1, 0, setpoint=self.set_point_height)
-        pid.output_limits = (0,6)   
+        pid.output_limits = (0, 6)
 
-        st = time.time() 
         while not self.stop_sim:
-            if time.time() - st > 1:
-                pid.setpoint = self.set_point_height
-                data = read_from_arduino()
-                
-                t_now     = time.time()
-                h = 0.18 - data[-1]
-                op = pid(h)
-                if arduino.is_open:
-                    arduino.write(f"{op}\n".encode())
-                st = t_now
-                self.real_pcs_hist.append((t_now,h_current))
-        pass
-    def pinn_sim(self):
-        pass
+            time.sleep(1)
+            data = read_from_arduino()
+            if data:
+                try:
+                    h = 0.18 - float(data)
+                    self.update_height.emit(h)
+                    op = pid(h)
+                    if arduino.is_open:
+                        arduino.write(f"{op}\n".encode())
+                except ValueError:
+                    print("Invalid data from Arduino")
 
-print("hi")
-ctrlr = Controller()
-ctrlr.set_point_height = 0.10
-ctrlr.run_fp_model_sim_async()
+    def stop(self):
+        self.stop_sim = True
+
+
+class PINNModelThread(QThread):
+    update_height = pyqtSignal(float)
+
+    def __init__(self, set_point_height=0.025):
+        super().__init__()
+        self.stop_sim = False
+        self.set_point_height = set_point_height
+
+    def run(self):
+        # Replace with PINN model code for prediction
+        h_current = 0.025  # Example initial height
+
+        while not self.stop_sim:
+            time.sleep(1)
+            # Calculate the new height using PINN
+            # Example: h_current = pinn_predict()
+            self.update_height.emit(h_current)
+
+    def stop(self):
+        self.stop_sim = True
---
+++ thread_test.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+# Define two functions to run in parallel
+def print_numbers():
+    for i in range(5):
+        print(f"Number: {i}")
+        time.sleep(1)
+
+def print_letters():
+    for letter in "ABCDE":
+        print(f"Letter: {letter}")
+        time.sleep(1)
+
+# Create two threads
+thread1 = threading.Thread(target=print_numbers)
+thread2 = threading.Thread(target=print_letters)
+
+# Start both threads
+thread1.start()
+thread2.start()
+
+# Wait for both threads to complete
+thread1.join()
+thread2.join()
+
+print("Both threads are complete!")
